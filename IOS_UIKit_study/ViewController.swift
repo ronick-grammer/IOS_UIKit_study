4a6a7a7b56dd5dfd307934836597a24d29150751
ronick-grammer <glflakcm@gmail.com>
2021-07-20 19:26:14 +0900

    챕터 3 미션!!

--- IOS_UIKit_study/ViewController.swift
+++ IOS_UIKit_study/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var current : Int = 1 // 첫 이미지
+    var imageName: String {
+        return String(current) + ".png"
+    }
+    
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var prev: UIButton!
+    @IBOutlet var nex: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imgView.image = UIImage(named: imageName)
     }
-
-
+    
+    @IBAction func btnPrev(_ sender: UIButton) {
+        if current > 1 { // 첫번쨰 이미지까지
+            current -= 1 //
+        }
+        imgView.image = UIImage(named: imageName)
+    }
+    
+    @IBAction func btnNext(_ sender: UIButton) {
+        if current < 3  { // 마지막 이미지까지
+            current += 1
+        }
+        
+        imgView.image = UIImage(named: imageName)
+    }
+    
+    
+    
 }
-
